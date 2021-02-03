7340d1b1a0864f62ae9b329a99e3fcf892480fe2
Adrien Treuille <adrien.g.treuille@gmail.com>
2021-02-03 10:58:36 -0800

    Almost done, but got into another off-by-one error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import random
 from typing import Set, Tuple
 from streamlit.session_state import SessionState
 
-Ships = Set[Tuple[int, int]]
+Point = Tuple[int, int]
+Points = Set[Point]
 
 # What I'm trying to do here is have the state reset if either the board
 # size of number of ships changed. In order to do so, I've added this
@@ -17,23 +18,12 @@ def config_parameter_changed(*_):
 
 def initialize_state(board_size: int, num_ships: int) -> None:
     """Setup a clean state of the board."""
-    state.board_size = board_size
-    state.num_ships = num_ships
+    state.board_size: int = board_size
+    state.num_ships: int = num_ships
+    state.guesses: Points = set()
+    state.current_guess: Optional[Point] = None
     create_new_random_board()
     state.initialized = True
-
-state = st.beta_session_state(initialized=False)
-board_size = st.sidebar.number_input("Board size", 5, 20, 10,
-        on_change=config_parameter_changed)
-num_ships = st.sidebar.number_input("Number of ships", 1, 10, 5,
-        on_change=config_parameter_changed)
-if not state.initialized:
-    initialize_state(board_size, num_ships)
-
-
-def get_settings() -> Tuple[int, int]:
-    """Gets some settings for the board."""
-    return board_size, num_ships
 
 def create_new_random_board() -> None:
     """Adds a new random board to the state."""
@@ -42,15 +32,22 @@ def create_new_random_board() -> None:
         ships = add_ship(ship_len, ships, state.board_size)
     state.ships = ships
 
-def add_ship(ship_len: int, ships: Ships, board_size) -> Ships:
+state = st.beta_session_state(initialized=False)
+
+def get_settings() -> Tuple[int, int]:
+    """Gets some settings for the board."""
+    return board_size, num_ships
+
+
+def add_ship(ship_len: int, ships: Points, board_size) -> Points:
     """Adds a ship of the specified length to the board."""
     MAX_ITER = 100
 
     # Stop trying to add the ship if it takes more than
     # MAX_ITER attempts to find a valid location
     for _ in range(MAX_ITER):
-        pos_1 = random.randrange(0, board_size - ship_len)
-        pos_2 = random.randrange(0, board_size)
+        pos_1 = random.randrange(1, board_size - ship_len + 1)
+        pos_2 = random.randrange(1, board_size + 1)
         horizontal = bool(random.randint(0, 1))
         if horizontal:
             new_ship = {(pos_1 + i, pos_2) for i in range(ship_len)}
@@ -60,31 +57,62 @@ def add_ship(ship_len: int, ships: Ships, board_size) -> Ships:
             return ships.union(new_ship)
     raise RuntimeError(f"Unable to place ship after {MAX_ITER} iterations.")
 
-def write_board(ships: Ships, board_size: int) -> None:
+def write_board(ships: Points, board_size: int) -> None:
     """Writes out the board to the Streamlit console."""
     st.sidebar.text("\n".join(
         " ".join("X" if (x, y) in ships else "."
-            for x in range(board_size))
-        for y in range(board_size)))
+            for x in range(1, board_size + 1))
+        for y in range(1, board_size + 1)))
 
-def click_cell(x: int, y:int) -> None:
+def click_cell(point: Point) -> None:
     """Returns a callback for when the specified cell is clicked."""
     def cell_clicked():
-        st.sidebar.info(f"Cell `{x}`x`{y}` clicked.")
+        state.current_guess = point
     return cell_clicked
 
 def draw_board(state: SessionState) -> None:
-     """Writes out the board to the Streamlit console."""
-     for y in range(1, state.board_size + 1):
-         row = st.beta_columns(state.board_size)
-         for x, cell in zip(range(1, state.board_size + 1), row):
-             cell.button(f"{x}x{y}", on_click=click_cell(x, y))
+ """Writes out the board to the Streamlit console."""
+ for y in range(1, state.board_size + 1):
+     row = st.beta_columns(state.board_size)
+     for x, cell in zip(range(1, state.board_size + 1), row):
+         point = (x, y)
+         if point not in state.guesses:
+             cell.button(f"{x}x{y}", on_click=click_cell(point))
+         elif point in state.ships:
+             cell.write("🔥")
+         else:
+             cell.write("🌊")
+
+def ask_for_answer() -> None:
+    """Prompt the user for the answer to the multiplication problem."""
+    if state.current_guess == None:
+        return
+    product_str = f"{state.current_guess[0]}X{state.current_guess[1]}"
+    st.info(f"What is {product_str}?")
+    guess = st.text_input(product_str)
+    if not guess:
+        return
+    if guess == str(state.current_guess[0] * state.current_guess[1]):
+        if state.current_guess in state.ships:
+            st.balloons()
+        st.success(f"{guess} is correct!")
+        state.guesses.add(state.current_guess)
+        state.current_guess = None
 
 def main():
     """Execution starts here."""
     st.write("# Battleship")
+
+    board_size = st.sidebar.number_input("Board size", 5, 20, 10,
+            on_change=config_parameter_changed)
+    num_ships = st.sidebar.number_input("Number of ships", 1, 10, 5,
+            on_change=config_parameter_changed)
+    if not state.initialized:
+        initialize_state(board_size, num_ships)
+
     write_board(state.ships, state.board_size)
     draw_board(state)
+    ask_for_answer()
 
 if __name__ == "__main__":
     main()
